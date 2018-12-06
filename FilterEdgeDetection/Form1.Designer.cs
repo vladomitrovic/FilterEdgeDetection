eeb1fa497d861ba39996a214f637f34387273398
Daniela <daniela.silvalourenco@students.hevs.ch>
2018-12-06 15:02:30 +0100

    test 2

--- FilterEdgeDetection/Form1.Designer.cs
+++ FilterEdgeDetection/Form1.Designer.cs
@@ -3,7 +3,7 @@
     partial class Form1
     {
         /// <summary>
-        /// Required designer variable.Push
+        /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
