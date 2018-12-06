5be2df4697540384ff0ae5b0811903fd0dc8c871
vladomitrovic <vlado.mitrovic@outlook.com>
2018-12-06 16:01:36 +0100

    I/O almost done

--- FilterEdgeDetection/IOfile/IPictureIO.cs
+++ FilterEdgeDetection/IOfile/IPictureIO.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace FilterEdgeDetection.IOfile
 {
-    class IPictureIO
+    interface IPictureIO
     {
+        Bitmap openPicrture(string path);
+
+        void savePicture(Bitmap savedPicture, string path);
     }
 }
---
+++ FilterEdgeDetection/IOfile/PictureIO.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilterEdgeDetection.IOfile
+{
+    class PictureIO
+    {
+        
+
+        public Bitmap openPicrture(string path)
+        {
+            Bitmap uploadedPicture;
+
+            StreamReader streamReader = new StreamReader(path);
+            uploadedPicture = (Bitmap)Bitmap.FromStream(streamReader.BaseStream);
+            streamReader.Close();
+
+            return uploadedPicture;
+        }
+
+        public void savePicture(Bitmap savedPicture, string path)
+        {
+
+            string fileExtension = Path.GetExtension(path).ToUpper();
+            ImageFormat imgFormat = ImageFormat.Png;
+
+            if (fileExtension == "BMP")
+            {
+                imgFormat = ImageFormat.Bmp;
+            }
+            else if (fileExtension == "JPG")
+            {
+                imgFormat = ImageFormat.Jpeg;
+            }
+
+            
+            StreamWriter streamWriter = new StreamWriter(path, false);
+            savedPicture.Save(streamWriter.BaseStream, imgFormat);
+            streamWriter.Flush();
+            streamWriter.Close();
+        }
+
+
+    }
+}
