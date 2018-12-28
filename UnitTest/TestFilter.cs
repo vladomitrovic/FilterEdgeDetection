f39cbab1d0acbfaf7ec2b5b0dc0fa1214e717d1c
vladomitrovic <vlado.mitrovic@outlook.com>
2018-12-28 16:11:57 +0100

    Start unit tests

---
+++ UnitTest/TestFilter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSubstitute;
+using FilterEdgeDetection.BLL;
+using System.Drawing;
+
+namespace UnitTest
+{
+
+    [TestClass]
+    public class TestFilter
+    {
+        [TestMethod]
+        public void TestBlackAndWhiteFilter()
+        {
+            var filter = Substitute.For<IPictureManipulation>();
+            Bitmap baseImage = new Bitmap("C:/Users/vlado/source/repos/FilterEdgeDetection/UnitTest/Resources/swissBase.png");
+            Bitmap exeptedImage = new Bitmap("C:/Users/vlado/source/repos/FilterEdgeDetection/UnitTest/Resources/swissBlackW.png");
+
+            baseImage = filter.BlackWhite(baseImage);
+
+            ImageComparaison.AreEqual(baseImage, exeptedImage);
+        }
+    }
+}
